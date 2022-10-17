808f6b9749bcdebc48da4e0acbab6aea76121d4e
Maria4lexzy <37138736+Maria4lexzy@users.noreply.github.com>
2022-10-17 17:54:06 +0200

    Bartender Section complete

---
+++ src/container/Bartender/Bartender.jsx
@@ -0,0 +1,33 @@
+/* eslint-disable jsx-a11y/alt-text */
+import React from 'react';
+import { SubHeading } from '../../components';
+import './Bartender.scss';
+import { pics } from '../../constants'
+const Bartender = () => (
+  <div className='app__bg app__wrapper section__padding'>
+    <div className='app__wrapper_img app__wrapper_img-reverse'>
+      <img src={pics.chef} alt="chef" />
+    </div>
+    <div className='app__wrapper_info'>
+      <SubHeading title="Bartender's Word" />
+      <h1 className='headtext__cormorant'>What we believe in</h1>
+
+      <div className='app__bartender-content'>
+        <div className='app__bartender-content_quote'>
+          <img src={pics.quote} alt="quote" />
+          <p className='p__opensans'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis, aliquid! Labore, aliquid. Totam quisquam quibusdam voluptas hic illo rerum libero quidem sed eius officiis.</p>
+        </div>
+        <p className='p__opensans'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis, aliquid! Labore, aliquid. Totam quisquam quibusdam voluptas hic illo rerum libero quidem sed eius officiis, minima inventore accusamus aliquam laboriosam harum</p>
+      </div>
+
+      <div className='app__bartender_sign'>
+        <p>Kevin Luo</p>
+        <p className='p__opensans'>Bartender & Founder</p>
+        <img src={pics.sign} alt="signature" />
+      </div>
+    </div>
+
+  </div>
+);
+
+export default Bartender;
