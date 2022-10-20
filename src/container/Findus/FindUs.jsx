416879d60f34d3e9617bd7aea9371254e84b20c7
Maria4lexzy <37138736+Maria4lexzy@users.noreply.github.com>
2022-10-20 12:56:35 +0200

    Components Updates
    Completed last sections of the webpage

--- src/container/Findus/FindUs.jsx
+++ src/container/Findus/FindUs.jsx
@@ -1,8 +1,22 @@
 import React from 'react';
-
+import { SubHeading } from '../../components';
+import { pics } from '../../constants';
 const FindUs = () => (
-  <div>
-    FindUs
+  <div className='app__bg app__wrapper section__padding' id='contact'>
+    <div className='app__wrapper_info'>
+      <SubHeading title="Contact" />
+      <h1 className='headtext__cormorant' style={{ marginBottm: '3rem' }}>Find us</h1>
+      <div className='app__wrapper-content'>
+        <p className='p__opensans'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem totam ad fuga voluptas veniam debitis minima magni aliquam, ducimus, repudiandae consequuntur non? Dolores, officiis? Debitis facilis distinctio nostrum laudantium perspiciatis.</p>
+        <p className='p__cormorant' style={{ color: "#dcca7", margin: '16px 0' }}>Opening Hours</p>
+        <p className='p__opensans'>Mon - Fri 18:00 - 23:00</p>
+        <p className='p__opensans'>Sat - Sun 16:00 - 00:00</p>
+      </div>
+      <div className='cusotm__button' style={{ marginTop: '16px' }}>Visit Us</div>
+    </div>
+    <div className='app__wrapper_img'>
+      <img src={pics.findus} alt="find us" />
+    </div>
   </div>
 );
 
--- src/container/Laurels/Laurels.jsx
+++ src/container/Laurels/Laurels.jsx
@@ -1,10 +1,36 @@
 import React from 'react';
-
-import './Laurels.css';
-
+import { SubHeading } from '../../components';
+import { pics, data } from '../../constants';
+import './Laurels.scss';
+const AwardCard = ({ award: { title, subtitle } }) => {
+  <div className='app__laurels_awards-card' key={title}>
+    {/* <img src={imgUrl} alt="award" /> */}
+    <div className='app__laurels_awards-card_content'>
+      <p className='p__cormorant' style={{ color: '#dcca87' }}>{title}</p>
+      <p className='p__cormorant' >{subtitle}</p>
+    </div>
+  </div>
+}
 const Laurels = () => (
-  <div>
-    Laurels
+  <div className='app__bg app__wrapper section__padding' id='awards'>
+    <div className='app__wrapper_info'>
+      <SubHeading title='Awards & Recognition' />
+      <h1 className='headtext__cormorant'>Our Laurels</h1>
+      <div className='app__laurels_awards'>
+
+        {
+          data.awards.map(award => {
+            console.log(award)
+            return <AwardCard award={award} key={award.title} />
+          })
+        }
+      </div>
+    </div>
+
+    <div className='app__wrapper_img'>
+      <img src={pics.laurels} alt="laurels" />
+
+    </div>
   </div>
 );
 
